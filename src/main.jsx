b413ed9f4834722031ee670c72e04fad6c672369
kaurkananreet <kaurkananreet@gmail.com>
2026-05-07 11:41:13 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react'
+import {useState} from 'react'
+import "./App.css"
+function App(){
+  const [name, setName] = useState("")
+  return(
+    <>
+      <h1>Enter your name:</h1>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <p>Your name is: {name}</p>
+    </>
+  )
+
+}
+
+export default App
+
