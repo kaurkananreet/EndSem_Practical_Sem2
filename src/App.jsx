9ccf1d559eae8e8b6a415270db43c90c3e08ea02
kaurkananreet <kaurkananreet@gmail.com>
2026-05-07 11:09:20 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
 // 1. Create an input field where a user enters their name. 
 // Display the entered name in real time below the input field as the user types.
-// 2. Create a react application demonstrating the use of the context API:
-// requirements:
-// - Create a context
-// -provide data from the parent component
-// -consume the data in a nested child component
 
 
 
-
-
-import React from 'react'
-import {useState} from 'react'
-import "./App.css"
-function App(){
-  return(
-    <>
-      <h1>Enter your name:</h1>
-      <input type="text" onChange={(e) => setName(e.target.value)} />
-      
-      <p>Your name is: {name}</p>
-    </>
-  )
-
+import { useState } from "react";
+function App() {
+ const [name, setName] = useState("");
+ return (
+   <div>
+     <input type="text" placeholder="Enter Name" onChange={(e) => setName(e.target.value)}/>
+     <h2>{name}</h2>
+   </div>
+ );
 }
+
+export default App;
